ca4cb75cf5b4b8a6fa9dec0f7fafe3d4f25d7981
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-24 20:26:29 +0200

    Set expiration time on gamesIDs

---
+++ src/lib/constants/cacheTime.ts
@@ -0,0 +1,1 @@
+export const GAME_IDS_CACHE_TIME = 50;
--- src/lib/helpers/randomGame.ts
+++ src/lib/helpers/randomGame.ts
@@ -1,3 +1,4 @@
+import { GAME_IDS_CACHE_TIME } from '$lib/constants/cacheTime';
 import { redis } from '$lib/constants/redis';
 import type { GameRegion } from '$lib/model/enums/gameRegion';
 
@@ -15,6 +16,8 @@ export const randomGame = async (region: GameRegion): Promise<number> => {
 		const gameIds: Array<number> = await gameIdsResponse.json();
 
 		redis.sadd(region, ...gameIds);
+		redis.expire(region, GAME_IDS_CACHE_TIME);
+
 		return gameIds[Math.floor(Math.random() * gameIds.length)];
 	} catch (e) {
 		console.log(e);
