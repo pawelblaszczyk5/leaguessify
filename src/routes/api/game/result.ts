cf31e698dca646faf74907b036ac49fe0d21b3c1
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-27 16:21:11 +0200

    Result endpoint

--- src/routes/api/game/result.ts
+++ src/routes/api/game/result.ts
@@ -1,3 +1,24 @@
-export const get = () => {
-	// TODO - Checking result of the game
+import type { GameRegion } from '$lib/api/model/enums/gameRegion';
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+import { handleError } from '$lib/api/helpers/handleError';
+import { getTeam } from '$lib/api/helpers/getTeam';
+
+export const get = async (request: Request): Promise<EndpointOutput> => {
+	const gameId = Number(request.query.get('gameId'));
+	const gameRegion: GameRegion = request.query.get('gameRegion') as GameRegion;
+	const winner = Number(request.query.get('winner'));
+
+	try {
+		const team = await getTeam(gameRegion, gameId, winner);
+
+		return {
+			status: 200,
+			body: {
+				result: team.win ? 'WIN' : 'LOST'
+			}
+		};
+	} catch (e) {
+		return handleError(e);
+	}
 };
