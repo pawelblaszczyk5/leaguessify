98b486624568392c5ee783adde3c56d933483ede
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-27 16:07:20 +0200

    Level endpoint

--- src/routes/api/game/participant/level.ts
+++ src/routes/api/game/participant/level.ts
@@ -1,3 +1,24 @@
-export const get = () => {
-	// TODO - getting level by participant
+import type { GameRegion } from '$lib/api/model/enums/gameRegion';
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+import { getParticipant } from '$lib/api/helpers/getParticipant';
+import { handleError } from '$lib/api/helpers/handleError';
+
+export const get = async (request: Request): Promise<EndpointOutput> => {
+	const gameId = Number(request.query.get('gameId'));
+	const gameRegion: GameRegion = request.query.get('gameRegion') as GameRegion;
+	const participantId = Number(request.query.get('participantId'));
+
+	try {
+		const participant = await getParticipant(gameRegion, gameId, participantId);
+
+		return {
+			status: 200,
+			body: {
+				level: participant.championLevel
+			}
+		};
+	} catch (e) {
+		return handleError(e);
+	}
 };
