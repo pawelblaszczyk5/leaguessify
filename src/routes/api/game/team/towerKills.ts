fa386a86f00808aa28fa9a949f6ba698f90eda50
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-27 16:17:57 +0200

    TowerKills endpoint

--- src/routes/api/game/team/towerKills.ts
+++ src/routes/api/game/team/towerKills.ts
@@ -1,3 +1,24 @@
-export const get = () => {
-	// TODO - getting tower kills count by team
+import type { GameRegion } from '$lib/api/model/enums/gameRegion';
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+import { handleError } from '$lib/api/helpers/handleError';
+import { getTeam } from '$lib/api/helpers/getTeam';
+
+export const get = async (request: Request): Promise<EndpointOutput> => {
+	const gameId = Number(request.query.get('gameId'));
+	const gameRegion: GameRegion = request.query.get('gameRegion') as GameRegion;
+	const teamId = Number(request.query.get('teamId'));
+
+	try {
+		const team = await getTeam(gameRegion, gameId, teamId);
+
+		return {
+			status: 200,
+			body: {
+				towerKills: team.towerKills
+			}
+		};
+	} catch (e) {
+		return handleError(e);
+	}
 };
