8b74ecf2c9c66f875edaa4b662f64d2bdd6a18d6
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-27 16:08:38 +0200

    Runes endpoint

--- src/routes/api/game/participant/runes.ts
+++ src/routes/api/game/participant/runes.ts
@@ -1,3 +1,25 @@
-export const get = () => {
-	// TODO - getting runes by participant
+import type { GameRegion } from '$lib/api/model/enums/gameRegion';
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+import { getParticipant } from '$lib/api/helpers/getParticipant';
+import { handleError } from '$lib/api/helpers/handleError';
+
+export const get = async (request: Request): Promise<EndpointOutput> => {
+	const gameId = Number(request.query.get('gameId'));
+	const gameRegion: GameRegion = request.query.get('gameRegion') as GameRegion;
+	const participantId = Number(request.query.get('participantId'));
+
+	try {
+		const participant = await getParticipant(gameRegion, gameId, participantId);
+
+		return {
+			status: 200,
+			body: {
+				keystone: participant.keystoneId,
+				secondaryRunePath: participant.secondaryRunePathId
+			}
+		};
+	} catch (e) {
+		return handleError(e);
+	}
 };
