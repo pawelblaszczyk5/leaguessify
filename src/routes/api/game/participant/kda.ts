d63483cc0bf0086b9ac5f0da981b69888ef00b17
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-27 16:06:55 +0200

    KDA endpoint

--- src/routes/api/game/participant/kda.ts
+++ src/routes/api/game/participant/kda.ts
@@ -1,3 +1,26 @@
-export const get = () => {
-	// TODO - getting KDA by participant
+import type { GameRegion } from '$lib/api/model/enums/gameRegion';
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+import { getParticipant } from '$lib/api/helpers/getParticipant';
+import { handleError } from '$lib/api/helpers/handleError';
+
+export const get = async (request: Request): Promise<EndpointOutput> => {
+	const gameId = Number(request.query.get('gameId'));
+	const gameRegion: GameRegion = request.query.get('gameRegion') as GameRegion;
+	const participantId = Number(request.query.get('participantId'));
+
+	try {
+		const participant = await getParticipant(gameRegion, gameId, participantId);
+
+		return {
+			status: 200,
+			body: {
+				kills: participant.kills,
+				deaths: participant.deaths,
+				assists: participant.assists
+			}
+		};
+	} catch (e) {
+		return handleError(e);
+	}
 };
