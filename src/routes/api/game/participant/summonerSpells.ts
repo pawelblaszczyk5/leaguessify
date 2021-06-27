f803688f126cb0330da50b8aa95f03761a9506d3
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-27 16:10:20 +0200

    SummonerSpells endpoint

--- src/routes/api/game/participant/summonerSpells.ts
+++ src/routes/api/game/participant/summonerSpells.ts
@@ -1,3 +1,25 @@
-export const get = () => {
-	// TODO - getting summoner spells by participant
+import type { GameRegion } from '$lib/api/model/enums/gameRegion';
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+import { getParticipant } from '$lib/api/helpers/getParticipant';
+import { handleError } from '$lib/api/helpers/handleError';
+
+export const get = async (request: Request): Promise<EndpointOutput> => {
+	const gameId = Number(request.query.get('gameId'));
+	const gameRegion: GameRegion = request.query.get('gameRegion') as GameRegion;
+	const participantId = Number(request.query.get('participantId'));
+
+	try {
+		const participant = await getParticipant(gameRegion, gameId, participantId);
+
+		return {
+			status: 200,
+			body: {
+				summonerSpell1: participant.summoner1Id,
+				summonerSpell2: participant.summoner2Id
+			}
+		};
+	} catch (e) {
+		return handleError(e);
+	}
 };
