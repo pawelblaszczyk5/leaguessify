37ed662c85ec3fedc5b7b91eebb31b89154fa93b
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-27 16:03:48 +0200

    Gold endpoint

--- src/routes/api/game/participant/gold.ts
+++ src/routes/api/game/participant/gold.ts
@@ -1,3 +1,23 @@
-export const get = () => {
-	// TODO - getting gold by participant
+import { getParticipant } from '$lib/api/helpers/getParticipant';
+import { handleError } from '$lib/api/helpers/handleError';
+import type { GameRegion } from '$lib/api/model/enums/gameRegion';
+import type { EndpointOutput, Request } from '@sveltejs/kit';
+
+export const get = async (request: Request): Promise<EndpointOutput> => {
+	const gameId = Number(request.query.get('gameId'));
+	const gameRegion: GameRegion = request.query.get('gameRegion') as GameRegion;
+	const participantId = Number(request.query.get('participantId'));
+
+	try {
+		const participant = await getParticipant(gameRegion, gameId, participantId);
+
+		return {
+			status: 200,
+			body: {
+				gold: participant.gold
+			}
+		};
+	} catch (e) {
+		return handleError(e);
+	}
 };
