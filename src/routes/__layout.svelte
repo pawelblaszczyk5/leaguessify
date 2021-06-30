162759eceb6d916c98ba225e577a2bcd639f213b
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-30 20:09:04 +0200

    Layout component, clean up main page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
+	import Nav from '$lib/components/Nav.svelte';
 	import { onMount } from 'svelte';
 	import '../app.postcss';
 	import oneLetterLogoSrc from '../assets/oneLetterLogo.webp';
 
-	onMount(() => {
+	const checkTheme = () => {
 		if (
 			localStorage.getItem('theme') === 'dark' ||
 			(!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)
@@ -12,11 +13,18 @@
 		} else {
 			document.documentElement.classList.remove('dark');
 		}
+	};
+
+	onMount(() => {
+		checkTheme();
 	});
 </script>
 
-<header class="flex p-4 justify-between">
-	<img class="w-16 h-16" src={oneLetterLogoSrc} alt="" />
+<header class="flex p-4 justify-between items-center">
+	<a href="/" title="Home page">
+		<img class="w-16 h-16" src={oneLetterLogoSrc} alt="" />
+	</a>
+	<Nav />
 </header>
 <main>
 	<slot />
