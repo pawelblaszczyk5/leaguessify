d820ed1efe8bad8d1ef0b5c45f6ca5a14ff1c816
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-06-30 20:23:01 +0200

    Simplify onMount function call

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
-	import { MenuIcon, XIcon } from 'svelte-feather-icons';
+	import { onMount } from 'svelte';
+	import { MenuIcon, XIcon, SunIcon, MoonIcon } from 'svelte-feather-icons';
 
 	let isMenuOpen = true;
+	let isDarkMode = false;
 
 	const toggleMenuState = () => {
 		isMenuOpen = !isMenuOpen;
 	};
+
+	const checkDarkModeStatus = () => {
+		isDarkMode = document.documentElement.classList.contains('dark');
+	};
+
+	const toggleDarkModeStatus = () => {
+		isDarkMode = !isDarkMode;
+		localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+		document.documentElement.classList.toggle('dark');
+	};
+
+	onMount(checkDarkModeStatus);
 </script>
 
-<nav>
-	<button
-		on:click={toggleMenuState}
-		class="focus:outline-none focus:ring-2 focus:ring-blue-500 sm:hidden"
-	>
-		<MenuIcon size="28" />
-	</button>
+<nav class="flex items-center">
 	<ul
 		class="transform-gpu fixed nm-flat-gray-200-sm dark:nm-flat-gray-800-sm h-screen w-80 max-w-full top-0 right-0 transition-transform duration-300 flex flex-col p-6 items-center text-xl {isMenuOpen
 			? 'translate-x-full'
@@ -48,4 +56,16 @@
 			>
 		</li>
 	</ul>
+	<button
+		on:click={toggleDarkModeStatus}
+		class="focus:outline-none focus:ring-2 focus:ring-blue-500 mr-4 sm:mr-0"
+	>
+		<svelte:component this={isDarkMode ? SunIcon : MoonIcon} size="28" />
+	</button>
+	<button
+		on:click={toggleMenuState}
+		class="focus:outline-none focus:ring-2 focus:ring-blue-500 sm:hidden"
+	>
+		<MenuIcon size="28" />
+	</button>
 </nav>
