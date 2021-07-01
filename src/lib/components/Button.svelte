991df315027e2b9a2f8c39fab798da89fe6e497f
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-01 20:24:29 +0200

    Button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,8 @@
+<button
+	on:click
+	type="button"
+	class="focus:outline-none focus focus:nm-concave-gray-200-lg dark:focus:nm-concave-gray-800-lg 
+    text-xl py-3 px-6 nm-flat-gray-200-lg dark:nm-flat-gray-800-lg hover:nm-inset-gray-200-lg dark:hover:nm-inset-gray-800-lg rounded-md"
+>
+	<slot>Click me!</slot>
+</button>
