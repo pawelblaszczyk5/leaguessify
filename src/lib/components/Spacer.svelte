9f9358d25f369d119b6f7374ccfd07d6590bdabf
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-01 20:24:43 +0200

    Spacer component, add spacing class to Tailwind safelist

---
+++ src/lib/components/Spacer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	export let x = 0;
+	export let y = 0;
+</script>
+
+<div class="my-{y} mx-{x}">
+	<slot />
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,32 @@ const config = {
 		}
 	},
 	darkMode: 'class',
-	plugins: [require('tailwindcss-neumorphism')]
+	plugins: [require('tailwindcss-neumorphism')],
+	purge: {
+		content: ['./src/**/*.svelte'],
+		safelist: [
+			'my-1',
+			'my-2',
+			'my-3',
+			'my-4',
+			'my-5',
+			'my-6',
+			'my-7',
+			'my-8',
+			'my-9',
+			'my-10',
+			'mx-1',
+			'mx-2',
+			'mx-3',
+			'mx-4',
+			'mx-5',
+			'mx-6',
+			'mx-7',
+			'mx-8',
+			'mx-9',
+			'mx-10'
+		]
+	}
 };
 
 module.exports = config;
