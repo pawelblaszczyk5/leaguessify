7d751cfedcc8e7558dabbb9dfae67929c375d366
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-04 14:37:02 +0200

    Summoner icons

---
+++ src/assets/summoner.ts
@@ -0,0 +1,31 @@
+import SummonerBarrier from './summoner/SummonerBarrier.png';
+import SummonerBoost from './summoner/SummonerBoost.png';
+import SummonerDot from './summoner/SummonerDot.png';
+import SummonerExhaust from './summoner/SummonerExhaust.png';
+import SummonerFlash from './summoner/SummonerFlash.png';
+import SummonerHaste from './summoner/SummonerHaste.png';
+import SummonerHeal from './summoner/SummonerHeal.png';
+import SummonerMana from './summoner/SummonerMana.png';
+import SummonerPoroRecall from './summoner/SummonerPoroRecall.png';
+import SummonerPoroThrow from './summoner/SummonerPoroThrow.png';
+import SummonerSmite from './summoner/SummonerSmite.png';
+import SummonerSnowball from './summoner/SummonerSnowball.png';
+import SummonerSnowURFSnowball_mark from './summoner/SummonerSnowURFSnowball_mark.png';
+import SummonerTeleport from './summoner/SummonerTeleport.png';
+
+export const summoner: Record<string, string> = {
+	SummonerBarrier,
+	SummonerBoost,
+	SummonerDot,
+	SummonerExhaust,
+	SummonerFlash,
+	SummonerHaste,
+	SummonerHeal,
+	SummonerMana,
+	SummonerPoroRecall,
+	SummonerPoroThrow,
+	SummonerSmite,
+	SummonerSnowball,
+	SummonerSnowURFSnowball_mark,
+	SummonerTeleport
+};
