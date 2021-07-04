4893d424bd21bb5c77c89e9f028e4bb23b517113
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-04 13:29:41 +0200

    Add item icons

---
+++ src/assets/itemIcon.ts
@@ -0,0 +1,447 @@
+import item1001 from './item/1001.png';
+import item1004 from './item/1004.png';
+import item1006 from './item/1006.png';
+import item1011 from './item/1011.png';
+import item1018 from './item/1018.png';
+import item1026 from './item/1026.png';
+import item1027 from './item/1027.png';
+import item1028 from './item/1028.png';
+import item1029 from './item/1029.png';
+import item1031 from './item/1031.png';
+import item1033 from './item/1033.png';
+import item1035 from './item/1035.png';
+import item1036 from './item/1036.png';
+import item1037 from './item/1037.png';
+import item1038 from './item/1038.png';
+import item1039 from './item/1039.png';
+import item1042 from './item/1042.png';
+import item1043 from './item/1043.png';
+import item1052 from './item/1052.png';
+import item1053 from './item/1053.png';
+import item1054 from './item/1054.png';
+import item1055 from './item/1055.png';
+import item1056 from './item/1056.png';
+import item1057 from './item/1057.png';
+import item1058 from './item/1058.png';
+import item1082 from './item/1082.png';
+import item1083 from './item/1083.png';
+import item2003 from './item/2003.png';
+import item2010 from './item/2010.png';
+import item2015 from './item/2015.png';
+import item2031 from './item/2031.png';
+import item2033 from './item/2033.png';
+import item2051 from './item/2051.png';
+import item2052 from './item/2052.png';
+import item2055 from './item/2055.png';
+import item2065 from './item/2065.png';
+import item2138 from './item/2138.png';
+import item2139 from './item/2139.png';
+import item2140 from './item/2140.png';
+import item2403 from './item/2403.png';
+import item2419 from './item/2419.png';
+import item2420 from './item/2420.png';
+import item2421 from './item/2421.png';
+import item2422 from './item/2422.png';
+import item2423 from './item/2423.png';
+import item2424 from './item/2424.png';
+import item3001 from './item/3001.png';
+import item3003 from './item/3003.png';
+import item3004 from './item/3004.png';
+import item3006 from './item/3006.png';
+import item3009 from './item/3009.png';
+import item3011 from './item/3011.png';
+import item3020 from './item/3020.png';
+import item3024 from './item/3024.png';
+import item3026 from './item/3026.png';
+import item3031 from './item/3031.png';
+import item3033 from './item/3033.png';
+import item3035 from './item/3035.png';
+import item3036 from './item/3036.png';
+import item3040 from './item/3040.png';
+import item3041 from './item/3041.png';
+import item3042 from './item/3042.png';
+import item3044 from './item/3044.png';
+import item3046 from './item/3046.png';
+import item3047 from './item/3047.png';
+import item3050 from './item/3050.png';
+import item3051 from './item/3051.png';
+import item3053 from './item/3053.png';
+import item3057 from './item/3057.png';
+import item3065 from './item/3065.png';
+import item3066 from './item/3066.png';
+import item3067 from './item/3067.png';
+import item3068 from './item/3068.png';
+import item3070 from './item/3070.png';
+import item3071 from './item/3071.png';
+import item3072 from './item/3072.png';
+import item3074 from './item/3074.png';
+import item3075 from './item/3075.png';
+import item3076 from './item/3076.png';
+import item3077 from './item/3077.png';
+import item3078 from './item/3078.png';
+import item3082 from './item/3082.png';
+import item3083 from './item/3083.png';
+import item3085 from './item/3085.png';
+import item3086 from './item/3086.png';
+import item3089 from './item/3089.png';
+import item3091 from './item/3091.png';
+import item3094 from './item/3094.png';
+import item3095 from './item/3095.png';
+import item3100 from './item/3100.png';
+import item3102 from './item/3102.png';
+import item3105 from './item/3105.png';
+import item3107 from './item/3107.png';
+import item3108 from './item/3108.png';
+import item3109 from './item/3109.png';
+import item3110 from './item/3110.png';
+import item3111 from './item/3111.png';
+import item3112 from './item/3112.png';
+import item3113 from './item/3113.png';
+import item3114 from './item/3114.png';
+import item3115 from './item/3115.png';
+import item3116 from './item/3116.png';
+import item3117 from './item/3117.png';
+import item3123 from './item/3123.png';
+import item3124 from './item/3124.png';
+import item3133 from './item/3133.png';
+import item3134 from './item/3134.png';
+import item3135 from './item/3135.png';
+import item3139 from './item/3139.png';
+import item3140 from './item/3140.png';
+import item3142 from './item/3142.png';
+import item3143 from './item/3143.png';
+import item3145 from './item/3145.png';
+import item3152 from './item/3152.png';
+import item3153 from './item/3153.png';
+import item3155 from './item/3155.png';
+import item3156 from './item/3156.png';
+import item3157 from './item/3157.png';
+import item3158 from './item/3158.png';
+import item3165 from './item/3165.png';
+import item3177 from './item/3177.png';
+import item3179 from './item/3179.png';
+import item3181 from './item/3181.png';
+import item3184 from './item/3184.png';
+import item3190 from './item/3190.png';
+import item3191 from './item/3191.png';
+import item3193 from './item/3193.png';
+import item3211 from './item/3211.png';
+import item3222 from './item/3222.png';
+import item3330 from './item/3330.png';
+import item3340 from './item/3340.png';
+import item3363 from './item/3363.png';
+import item3364 from './item/3364.png';
+import item3400 from './item/3400.png';
+import item3504 from './item/3504.png';
+import item3508 from './item/3508.png';
+import item3513 from './item/3513.png';
+import item3599 from './item/3599.png';
+import item3600 from './item/3600.png';
+import item3742 from './item/3742.png';
+import item3748 from './item/3748.png';
+import item3801 from './item/3801.png';
+import item3802 from './item/3802.png';
+import item3814 from './item/3814.png';
+import item3850 from './item/3850.png';
+import item3851 from './item/3851.png';
+import item3853 from './item/3853.png';
+import item3854 from './item/3854.png';
+import item3855 from './item/3855.png';
+import item3857 from './item/3857.png';
+import item3858 from './item/3858.png';
+import item3859 from './item/3859.png';
+import item3860 from './item/3860.png';
+import item3862 from './item/3862.png';
+import item3863 from './item/3863.png';
+import item3864 from './item/3864.png';
+import item3916 from './item/3916.png';
+import item4005 from './item/4005.png';
+import item4401 from './item/4401.png';
+import item4403 from './item/4403.png';
+import item4628 from './item/4628.png';
+import item4629 from './item/4629.png';
+import item4630 from './item/4630.png';
+import item4632 from './item/4632.png';
+import item4633 from './item/4633.png';
+import item4635 from './item/4635.png';
+import item4636 from './item/4636.png';
+import item4637 from './item/4637.png';
+import item4638 from './item/4638.png';
+import item4642 from './item/4642.png';
+import item4643 from './item/4643.png';
+import item6029 from './item/6029.png';
+import item6035 from './item/6035.png';
+import item6333 from './item/6333.png';
+import item6609 from './item/6609.png';
+import item6616 from './item/6616.png';
+import item6617 from './item/6617.png';
+import item6630 from './item/6630.png';
+import item6631 from './item/6631.png';
+import item6632 from './item/6632.png';
+import item6653 from './item/6653.png';
+import item6655 from './item/6655.png';
+import item6656 from './item/6656.png';
+import item6660 from './item/6660.png';
+import item6662 from './item/6662.png';
+import item6664 from './item/6664.png';
+import item6670 from './item/6670.png';
+import item6671 from './item/6671.png';
+import item6672 from './item/6672.png';
+import item6673 from './item/6673.png';
+import item6675 from './item/6675.png';
+import item6676 from './item/6676.png';
+import item6677 from './item/6677.png';
+import item6691 from './item/6691.png';
+import item6692 from './item/6692.png';
+import item6693 from './item/6693.png';
+import item6694 from './item/6694.png';
+import item6695 from './item/6695.png';
+import item7000 from './item/7000.png';
+import item7001 from './item/7001.png';
+import item7002 from './item/7002.png';
+import item7003 from './item/7003.png';
+import item7004 from './item/7004.png';
+import item7005 from './item/7005.png';
+import item7006 from './item/7006.png';
+import item7007 from './item/7007.png';
+import item7008 from './item/7008.png';
+import item7009 from './item/7009.png';
+import item7010 from './item/7010.png';
+import item7011 from './item/7011.png';
+import item7012 from './item/7012.png';
+import item7013 from './item/7013.png';
+import item7014 from './item/7014.png';
+import item7015 from './item/7015.png';
+import item7016 from './item/7016.png';
+import item7017 from './item/7017.png';
+import item7018 from './item/7018.png';
+import item7019 from './item/7019.png';
+import item7020 from './item/7020.png';
+import item7021 from './item/7021.png';
+import item7022 from './item/7022.png';
+import item8001 from './item/8001.png';
+
+export const itemIcon: Record<string, string> = {
+	item1001,
+	item1004,
+	item1006,
+	item1011,
+	item1018,
+	item1026,
+	item1027,
+	item1028,
+	item1029,
+	item1031,
+	item1033,
+	item1035,
+	item1036,
+	item1037,
+	item1038,
+	item1039,
+	item1042,
+	item1043,
+	item1052,
+	item1053,
+	item1054,
+	item1055,
+	item1056,
+	item1057,
+	item1058,
+	item1082,
+	item1083,
+	item2003,
+	item2010,
+	item2015,
+	item2031,
+	item2033,
+	item2051,
+	item2052,
+	item2055,
+	item2065,
+	item2138,
+	item2139,
+	item2140,
+	item2403,
+	item2419,
+	item2420,
+	item2421,
+	item2422,
+	item2423,
+	item2424,
+	item3001,
+	item3003,
+	item3004,
+	item3006,
+	item3009,
+	item3011,
+	item3020,
+	item3024,
+	item3026,
+	item3031,
+	item3033,
+	item3035,
+	item3036,
+	item3040,
+	item3041,
+	item3042,
+	item3044,
+	item3046,
+	item3047,
+	item3050,
+	item3051,
+	item3053,
+	item3057,
+	item3065,
+	item3066,
+	item3067,
+	item3068,
+	item3070,
+	item3071,
+	item3072,
+	item3074,
+	item3075,
+	item3076,
+	item3077,
+	item3078,
+	item3082,
+	item3083,
+	item3085,
+	item3086,
+	item3089,
+	item3091,
+	item3094,
+	item3095,
+	item3100,
+	item3102,
+	item3105,
+	item3107,
+	item3108,
+	item3109,
+	item3110,
+	item3111,
+	item3112,
+	item3113,
+	item3114,
+	item3115,
+	item3116,
+	item3117,
+	item3123,
+	item3124,
+	item3133,
+	item3134,
+	item3135,
+	item3139,
+	item3140,
+	item3142,
+	item3143,
+	item3145,
+	item3152,
+	item3153,
+	item3155,
+	item3156,
+	item3157,
+	item3158,
+	item3165,
+	item3177,
+	item3179,
+	item3181,
+	item3184,
+	item3190,
+	item3191,
+	item3193,
+	item3211,
+	item3222,
+	item3330,
+	item3340,
+	item3363,
+	item3364,
+	item3400,
+	item3504,
+	item3508,
+	item3513,
+	item3599,
+	item3600,
+	item3742,
+	item3748,
+	item3801,
+	item3802,
+	item3814,
+	item3850,
+	item3851,
+	item3853,
+	item3854,
+	item3855,
+	item3857,
+	item3858,
+	item3859,
+	item3860,
+	item3862,
+	item3863,
+	item3864,
+	item3916,
+	item4005,
+	item4401,
+	item4403,
+	item4628,
+	item4629,
+	item4630,
+	item4632,
+	item4633,
+	item4635,
+	item4636,
+	item4637,
+	item4638,
+	item4642,
+	item4643,
+	item6029,
+	item6035,
+	item6333,
+	item6609,
+	item6616,
+	item6617,
+	item6630,
+	item6631,
+	item6632,
+	item6653,
+	item6655,
+	item6656,
+	item6660,
+	item6662,
+	item6664,
+	item6670,
+	item6671,
+	item6672,
+	item6673,
+	item6675,
+	item6676,
+	item6677,
+	item6691,
+	item6692,
+	item6693,
+	item6694,
+	item6695,
+	item7000,
+	item7001,
+	item7002,
+	item7003,
+	item7004,
+	item7005,
+	item7006,
+	item7007,
+	item7008,
+	item7009,
+	item7010,
+	item7011,
+	item7012,
+	item7013,
+	item7014,
+	item7015,
+	item7016,
+	item7017,
+	item7018,
+	item7019,
+	item7020,
+	item7021,
+	item7022,
+	item8001
+};
