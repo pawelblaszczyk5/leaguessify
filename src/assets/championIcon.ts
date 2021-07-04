f366cbafa72ad9e28d7617f1cbf193fb3432d7d1
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-04 12:38:45 +0200

    Add champion icons

---
+++ src/assets/championIcon.ts
@@ -0,0 +1,313 @@
+import Aatrox from './champion/Aatrox.png';
+import Ahri from './champion/Ahri.png';
+import Akali from './champion/Akali.png';
+import Alistar from './champion/Alistar.png';
+import Amumu from './champion/Amumu.png';
+import Anivia from './champion/Anivia.png';
+import Annie from './champion/Annie.png';
+import Aphelios from './champion/Aphelios.png';
+import Ashe from './champion/Ashe.png';
+import AurelionSol from './champion/AurelionSol.png';
+import Azir from './champion/Azir.png';
+import Bard from './champion/Bard.png';
+import Blitzcrank from './champion/Blitzcrank.png';
+import Brand from './champion/Brand.png';
+import Braum from './champion/Braum.png';
+import Caitlyn from './champion/Caitlyn.png';
+import Camille from './champion/Camille.png';
+import Cassiopeia from './champion/Cassiopeia.png';
+import Chogath from './champion/Chogath.png';
+import Corki from './champion/Corki.png';
+import Darius from './champion/Darius.png';
+import Diana from './champion/Diana.png';
+import Draven from './champion/Draven.png';
+import DrMundo from './champion/DrMundo.png';
+import Ekko from './champion/Ekko.png';
+import Elise from './champion/Elise.png';
+import Evelynn from './champion/Evelynn.png';
+import Ezreal from './champion/Ezreal.png';
+import Fiddlesticks from './champion/Fiddlesticks.png';
+import Fiora from './champion/Fiora.png';
+import Fizz from './champion/Fizz.png';
+import Galio from './champion/Galio.png';
+import Gangplank from './champion/Gangplank.png';
+import Garen from './champion/Garen.png';
+import Gnar from './champion/Gnar.png';
+import Gragas from './champion/Gragas.png';
+import Graves from './champion/Graves.png';
+import Gwen from './champion/Gwen.png';
+import Hecarim from './champion/Hecarim.png';
+import Heimerdinger from './champion/Heimerdinger.png';
+import Illaoi from './champion/Illaoi.png';
+import Irelia from './champion/Irelia.png';
+import Ivern from './champion/Ivern.png';
+import Janna from './champion/Janna.png';
+import JarvanIV from './champion/JarvanIV.png';
+import Jax from './champion/Jax.png';
+import Jayce from './champion/Jayce.png';
+import Jhin from './champion/Jhin.png';
+import Jinx from './champion/Jinx.png';
+import Kaisa from './champion/Kaisa.png';
+import Kalista from './champion/Kalista.png';
+import Karma from './champion/Karma.png';
+import Karthus from './champion/Karthus.png';
+import Kassadin from './champion/Kassadin.png';
+import Katarina from './champion/Katarina.png';
+import Kayle from './champion/Kayle.png';
+import Kayn from './champion/Kayn.png';
+import Kennen from './champion/Kennen.png';
+import Khazix from './champion/Khazix.png';
+import Kindred from './champion/Kindred.png';
+import Kled from './champion/Kled.png';
+import KogMaw from './champion/KogMaw.png';
+import Leblanc from './champion/Leblanc.png';
+import LeeSin from './champion/LeeSin.png';
+import Leona from './champion/Leona.png';
+import Lillia from './champion/Lillia.png';
+import Lissandra from './champion/Lissandra.png';
+import Lucian from './champion/Lucian.png';
+import Lulu from './champion/Lulu.png';
+import Lux from './champion/Lux.png';
+import Malphite from './champion/Malphite.png';
+import Malzahar from './champion/Malzahar.png';
+import Maokai from './champion/Maokai.png';
+import MasterYi from './champion/MasterYi.png';
+import MissFortune from './champion/MissFortune.png';
+import MonkeyKing from './champion/MonkeyKing.png';
+import Mordekaiser from './champion/Mordekaiser.png';
+import Morgana from './champion/Morgana.png';
+import Nami from './champion/Nami.png';
+import Nasus from './champion/Nasus.png';
+import Nautilus from './champion/Nautilus.png';
+import Neeko from './champion/Neeko.png';
+import Nidalee from './champion/Nidalee.png';
+import Nocturne from './champion/Nocturne.png';
+import Nunu from './champion/Nunu.png';
+import Olaf from './champion/Olaf.png';
+import Orianna from './champion/Orianna.png';
+import Ornn from './champion/Ornn.png';
+import Pantheon from './champion/Pantheon.png';
+import Poppy from './champion/Poppy.png';
+import Pyke from './champion/Pyke.png';
+import Qiyana from './champion/Qiyana.png';
+import Quinn from './champion/Quinn.png';
+import Rakan from './champion/Rakan.png';
+import Rammus from './champion/Rammus.png';
+import RekSai from './champion/RekSai.png';
+import Rell from './champion/Rell.png';
+import Renekton from './champion/Renekton.png';
+import Rengar from './champion/Rengar.png';
+import Riven from './champion/Riven.png';
+import Rumble from './champion/Rumble.png';
+import Ryze from './champion/Ryze.png';
+import Samira from './champion/Samira.png';
+import Sejuani from './champion/Sejuani.png';
+import Senna from './champion/Senna.png';
+import Seraphine from './champion/Seraphine.png';
+import Sett from './champion/Sett.png';
+import Shaco from './champion/Shaco.png';
+import Shen from './champion/Shen.png';
+import Shyvana from './champion/Shyvana.png';
+import Singed from './champion/Singed.png';
+import Sion from './champion/Sion.png';
+import Sivir from './champion/Sivir.png';
+import Skarner from './champion/Skarner.png';
+import Sona from './champion/Sona.png';
+import Soraka from './champion/Soraka.png';
+import Swain from './champion/Swain.png';
+import Sylas from './champion/Sylas.png';
+import Syndra from './champion/Syndra.png';
+import TahmKench from './champion/TahmKench.png';
+import Taliyah from './champion/Taliyah.png';
+import Talon from './champion/Talon.png';
+import Taric from './champion/Taric.png';
+import Teemo from './champion/Teemo.png';
+import Thresh from './champion/Thresh.png';
+import Tristana from './champion/Tristana.png';
+import Trundle from './champion/Trundle.png';
+import Tryndamere from './champion/Tryndamere.png';
+import TwistedFate from './champion/TwistedFate.png';
+import Twitch from './champion/Twitch.png';
+import Udyr from './champion/Udyr.png';
+import Urgot from './champion/Urgot.png';
+import Varus from './champion/Varus.png';
+import Vayne from './champion/Vayne.png';
+import Veigar from './champion/Veigar.png';
+import Velkoz from './champion/Velkoz.png';
+import Vi from './champion/Vi.png';
+import Viego from './champion/Viego.png';
+import Viktor from './champion/Viktor.png';
+import Vladimir from './champion/Vladimir.png';
+import Volibear from './champion/Volibear.png';
+import Warwick from './champion/Warwick.png';
+import Xayah from './champion/Xayah.png';
+import Xerath from './champion/Xerath.png';
+import XinZhao from './champion/XinZhao.png';
+import Yasuo from './champion/Yasuo.png';
+import Yone from './champion/Yone.png';
+import Yorick from './champion/Yorick.png';
+import Yuumi from './champion/Yuumi.png';
+import Zac from './champion/Zac.png';
+import Zed from './champion/Zed.png';
+import Ziggs from './champion/Ziggs.png';
+import Zilean from './champion/Zilean.png';
+import Zoe from './champion/Zoe.png';
+import Zyra from './champion/Zyra.png';
+
+export const championIcon: Record<string, string> = {
+	Aatrox,
+	Ahri,
+	Akali,
+	Alistar,
+	Amumu,
+	Anivia,
+	Annie,
+	Aphelios,
+	Ashe,
+	AurelionSol,
+	Azir,
+	Bard,
+	Blitzcrank,
+	Brand,
+	Braum,
+	Caitlyn,
+	Camille,
+	Cassiopeia,
+	Chogath,
+	Corki,
+	Darius,
+	Diana,
+	Draven,
+	DrMundo,
+	Ekko,
+	Elise,
+	Evelynn,
+	Ezreal,
+	Fiddlesticks,
+	Fiora,
+	Fizz,
+	Galio,
+	Gangplank,
+	Garen,
+	Gnar,
+	Gragas,
+	Graves,
+	Gwen,
+	Hecarim,
+	Heimerdinger,
+	Illaoi,
+	Irelia,
+	Ivern,
+	Janna,
+	JarvanIV,
+	Jax,
+	Jayce,
+	Jhin,
+	Jinx,
+	Kaisa,
+	Kalista,
+	Karma,
+	Karthus,
+	Kassadin,
+	Katarina,
+	Kayle,
+	Kayn,
+	Kennen,
+	Khazix,
+	Kindred,
+	Kled,
+	KogMaw,
+	Leblanc,
+	LeeSin,
+	Leona,
+	Lillia,
+	Lissandra,
+	Lucian,
+	Lulu,
+	Lux,
+	Malphite,
+	Malzahar,
+	Maokai,
+	MasterYi,
+	MissFortune,
+	MonkeyKing,
+	Mordekaiser,
+	Morgana,
+	Nami,
+	Nasus,
+	Nautilus,
+	Neeko,
+	Nidalee,
+	Nocturne,
+	Nunu,
+	Olaf,
+	Orianna,
+	Ornn,
+	Pantheon,
+	Poppy,
+	Pyke,
+	Qiyana,
+	Quinn,
+	Rakan,
+	Rammus,
+	RekSai,
+	Rell,
+	Renekton,
+	Rengar,
+	Riven,
+	Rumble,
+	Ryze,
+	Samira,
+	Sejuani,
+	Senna,
+	Seraphine,
+	Sett,
+	Shaco,
+	Shen,
+	Shyvana,
+	Singed,
+	Sion,
+	Sivir,
+	Skarner,
+	Sona,
+	Soraka,
+	Swain,
+	Sylas,
+	Syndra,
+	TahmKench,
+	Taliyah,
+	Talon,
+	Taric,
+	Teemo,
+	Thresh,
+	Tristana,
+	Trundle,
+	Tryndamere,
+	TwistedFate,
+	Twitch,
+	Udyr,
+	Urgot,
+	Varus,
+	Vayne,
+	Veigar,
+	Velkoz,
+	Vi,
+	Viego,
+	Viktor,
+	Vladimir,
+	Volibear,
+	Warwick,
+	Xayah,
+	Xerath,
+	XinZhao,
+	Yasuo,
+	Yone,
+	Yorick,
+	Yuumi,
+	Zac,
+	Zed,
+	Ziggs,
+	Zilean,
+	Zoe,
+	Zyra
+};
