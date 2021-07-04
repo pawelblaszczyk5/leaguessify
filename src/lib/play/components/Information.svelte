f80bf524a77cc8eeeeb7fb712def91aa85e1c797
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-04 20:17:38 +0200

    KDA and gold added

---
+++ src/lib/play/components/Information.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+	import Icon from '$lib/shared/components/Icon.svelte';
+	import IoIosStats from 'svelte-icons/io/IoIosStats.svelte';
+	import FaCoins from 'svelte-icons/fa/FaCoins.svelte';
+	import { get } from 'svelte/store';
+	import { game } from '../stores/game';
+
+	export let kda: [number, number, number];
+	export let gold: number;
+	export let participantId: number;
+
+	const revealGold = async () => {
+		const gameData = get(game);
+		const params = new URLSearchParams({
+			gameId: gameData.id.toString(),
+			gameRegion: gameData.region,
+			participantId: participantId.toString()
+		}).toString();
+
+		try {
+			const goldResponse = await fetch(`api/game/participant/gold?${params}`);
+			const goldData: { gold: number } = await goldResponse.json();
+
+			gold = goldData.gold;
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
+	const revealKDA = async () => {
+		const gameData = get(game);
+		const params = new URLSearchParams({
+			gameId: gameData.id.toString(),
+			gameRegion: gameData.region,
+			participantId: participantId.toString()
+		}).toString();
+
+		try {
+			const kdaResponse = await fetch(`api/game/participant/kda?${params}`);
+			const kdaData: { kills: number; deaths: number; assists: number } = await kdaResponse.json();
+
+			kda = [kdaData.kills, kdaData.deaths, kdaData.assists];
+		} catch (e) {
+			console.log(e);
+		}
+	};
+</script>
+
+<div class="mx-2 text-sm">
+	<div class="flex items-center">
+		<div class="flex items-center relative">
+			{#if isNaN(kda[0])}
+				<button
+					on:click={revealKDA}
+					class="w-full h-full absolute flex items-center justify-center bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-80"
+				>
+					Reveal
+				</button>
+			{/if}
+			<Icon --size="14px">
+				<IoIosStats />
+			</Icon>
+			<span> &nbsp; {!isNaN(kda[0]) ? `${kda[0]}/${kda[1]}/${kda[2]}` : '?/?/?'} </span>
+		</div>
+		<div class="flex items-center ml-2 relative">
+			{#if isNaN(gold)}
+				<button
+					on:click={revealGold}
+					class="w-full h-full absolute flex items-center justify-center bg-gray-200 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-80"
+				>
+					Reveal
+				</button>
+			{/if}
+			<Icon --size="14px">
+				<FaCoins />
+			</Icon>
+			<div />
+			<span> &nbsp; {gold ?? '?????'}</span>
+		</div>
+	</div>
+</div>
--- src/lib/play/components/SingleParticipant.svelte
+++ src/lib/play/components/SingleParticipant.svelte
@@ -6,26 +6,34 @@
 	import Summoners from './Summoners.svelte';
 	import Spacer from '$lib/shared/components/Spacer.svelte';
 	import Runes from './Runes.svelte';
+	import Information from './Information.svelte';
 
 	export let participant: Participant;
 	export let reversed = false;
 </script>
 
-<div class="flex m-3" class:flex-row-reverse={reversed}>
+<div class="flex mx-3 my-4" class:flex-row-reverse={reversed}>
 	<Spacer>
-		<Runes
-			runes={[participant.keystoneId, participant.secondaryRunePathId]}
-			participantId={participant.championId}
-		/>
-	</Spacer>
-	<Spacer x={3}>
 		<Summoners
 			participantId={participant.championId}
 			summoners={[participant.summoner1Id, participant.summoner2Id]}
 		/>
 	</Spacer>
+	<Spacer x={3}>
+		<Runes
+			runes={[participant.keystoneId, participant.secondaryRunePathId]}
+			participantId={participant.championId}
+		/>
+	</Spacer>
 	<div class="nm-flat-gray-200-lg dark:nm-flat-gray-800-lg w-16 h-16 rounded-lg overflow-hidden">
 		<img src={championIcon[participant.championName]} alt="" />
 	</div>
-	<Items participantId={participant.championId} items={participant.items} {reversed} />
+	<div class="flex justify-between flex-col" class:items-end={reversed}>
+		<Items participantId={participant.championId} items={participant.items} {reversed} />
+		<Information
+			kda={[participant.kills, participant.deaths, participant.assists]}
+			gold={participant.gold}
+			participantId={participant.championId}
+		/>
+	</div>
 </div>
