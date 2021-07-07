a26cb29e4d63a3e7afff6c6d8526868b2f0b749c
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-07 19:01:42 +0200

    Add update and conditionally disabling to tooltip action

--- src/lib/shared/actions/tooltip.ts
+++ src/lib/shared/actions/tooltip.ts
@@ -2,8 +2,37 @@ import tippy from 'tippy.js';
 
 interface TooltipConfig {
 	content: string;
+	shouldShow?: boolean;
 }
 
-export const tooltip = (node: HTMLElement, config: TooltipConfig): void => {
-	tippy(node, { ...config, theme: 'blue' });
+interface TooltipActionReturn {
+	update: (config: TooltipConfig) => void;
+	destroy: () => void;
+}
+
+const defaultTippyConfig = {
+	theme: 'blue'
+};
+
+export const tooltip = (node: HTMLElement, config: TooltipConfig): TooltipActionReturn => {
+	const tippyInstance = tippy(node, { content: config.content, ...defaultTippyConfig });
+
+	if (config.shouldShow === false) {
+		tippyInstance.disable();
+	}
+
+	return {
+		update: (config: TooltipConfig) => {
+			tippyInstance.setProps({ ...config, ...defaultTippyConfig });
+
+			if (config.shouldShow === false) {
+				tippyInstance.disable();
+			} else {
+				tippyInstance.enable();
+			}
+		},
+		destroy: () => {
+			tippyInstance.unmount();
+		}
+	};
 };
