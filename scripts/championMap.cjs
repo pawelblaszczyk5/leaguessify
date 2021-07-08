151cc2dd1fc4d5fc6f2bd825be4b5bbaf5c8dee7
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-08 20:35:57 +0200

    Fix for Fiddlesticks id mismatch in Riot data/API

--- scripts/championMap.cjs
+++ scripts/championMap.cjs
@@ -1,10 +1,18 @@
 let champions = require('./data/champion.json');
 let fs = require('fs');
 
+const mapChampionId = (id) => {
+	if (id === 'Fiddlesticks') {
+		return 'FiddleSticks';
+	}
+
+	return id;
+};
+
 const mappedChampionData = Object.fromEntries(
 	Object.entries(champions.data).map(([championId, championObject]) => {
 		return [
-			championId,
+			mapChampionId(championId),
 			{
 				name: championObject.name,
 				title: championObject.title
