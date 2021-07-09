71591f0c5d3ba008571ccab8554227e63029d639
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-09 21:07:25 +0200

    Scoring balance adjustments

--- src/lib/shared/constants/revealPrices.ts
+++ src/lib/shared/constants/revealPrices.ts
@@ -2,16 +2,16 @@ import { RevealType } from '../model/enums/revealType';
 
 export const revealPrices: Record<RevealType, number> = {
 	[RevealType.DURATION]: 200,
-	[RevealType.CHAMPION_KILLS]: 400,
-	[RevealType.TOWER_KILLS]: 800,
-	[RevealType.DRAGON_KILLS]: 600,
+	[RevealType.CHAMPION_KILLS]: 300,
+	[RevealType.TOWER_KILLS]: 750,
+	[RevealType.DRAGON_KILLS]: 500,
 	[RevealType.RIFT_HERALD_KILLS]: 300,
-	[RevealType.BARON_NASHOR_KILLS]: 700,
-	[RevealType.INHIBITOR_KILLS]: 900,
+	[RevealType.BARON_NASHOR_KILLS]: 800,
+	[RevealType.INHIBITOR_KILLS]: 950,
 	[RevealType.SUMMONERS]: 100,
 	[RevealType.RUNES]: 100,
-	[RevealType.CHAMPION_LEVEL]: 200,
+	[RevealType.CHAMPION_LEVEL]: 300,
 	[RevealType.ITEMS]: 400,
-	[RevealType.KDA]: 500,
+	[RevealType.KDA]: 450,
 	[RevealType.GOLD]: 300
 };
