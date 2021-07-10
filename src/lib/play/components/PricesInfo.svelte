c486f5030a4b1349294de174b97a6c629bc77523
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-10 08:12:17 +0200

    Fix color contrast

--- src/lib/play/components/PricesInfo.svelte
+++ src/lib/play/components/PricesInfo.svelte
@@ -42,8 +42,11 @@
 	<h2 class="text-lg">Reveal prices:</h2>
 	{#each revealTypes as revealType (revealType)}
 		<p class="my-1">
-			{revealNames[revealType]}: <span class="text-blue-500">{revealPrices[revealType]}</span>
+			{revealNames[revealType]}:
+			<span class="text-blue-700 dark:text-blue-300">{revealPrices[revealType]}</span>
 		</p>
 	{/each}
-	<h2 class="text-lg">Guessing prize: <span class="text-blue-500">{WIN_PRIZE}</span></h2>
+	<h2 class="text-lg">
+		Guessing prize: <span class="text-blue-700 dark:text-blue-300">{WIN_PRIZE}</span>
+	</h2>
 </div>
