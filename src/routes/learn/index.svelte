b872375b90e8aa21f545bc3ec692f60a17af3be7
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-11 10:57:58 +0200

    Learn page added

---
+++ src/routes/learn/index.svelte
@@ -0,0 +1,29 @@
+<section class="flex flex-col items-center justify-center">
+	<h1 class="text-center text-2xl max-w-lg">Goal of the game</h1>
+	<p class="text-center max-w-lg my-3">
+		The goal is to get an as high score as possible by guessing the winner of the game with as
+		little information as possible. Gather the points through correct picking and then spend it to
+		gather various info in the next round. Some knowledge is more valuable than others, so costs
+		vary to balance everything out.
+	</p>
+</section>
+<section class="flex flex-col items-center justify-center">
+	<h1 class="text-center text-2xl max-w-lg">How to play?</h1>
+	<p class="text-center max-w-lg my-3">
+		Use the reveal buttons to spend your score and get valuable pieces of information. If you are
+		not sure what is hiding below, there is a friendly tooltip for you. Hover it (or hold your
+		finger if you are on a touch device), and you will never be confused. At every point of the
+		game, you can check how much everything costs. It's simple - just click the info button in the
+		bottom-right corner. Finally, if you are feeling confident or you have no other choice - make a
+		guess. Click the button below the corresponding team to do it.
+	</p>
+</section>
+<section class="flex flex-col items-center justify-center">
+	<h1 class="text-center text-2xl max-w-lg">What games can you expect there?</h1>
+	<p class="text-center max-w-lg my-3">
+		Games are taken from 5 servers - EUNE, EUW, KR, NA, and JP, from various ranks. The data size is
+		pretty generous. You can expect a variety of puzzles. Moreover, games are rotated every few
+		days, so you will never know what awaits behind a corner. Maybe you will even recognize your own
+		match?
+	</p>
+</section>
