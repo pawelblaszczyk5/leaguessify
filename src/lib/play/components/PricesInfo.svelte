1f037cbb8364c4cb885ae174eb0ccb7ec13a2d1c
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-11 09:56:35 +0200

    Move PricesInfo into separate component from modal

--- src/lib/play/components/PricesInfo.svelte
+++ src/lib/play/components/PricesInfo.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import Icon from '$lib/shared/components/Icon.svelte';
-	import { revealNames } from '$lib/shared/constants/revealNames';
-	import { revealPrices } from '$lib/shared/constants/revealPrices';
-	import { WIN_PRIZE } from '$lib/shared/constants/winPrize';
-	import { RevealType } from '$lib/shared/model/enums/revealType';
+	import PricesInfoContent from '$lib/shared/components/PricesInfoContent.svelte';
 	import { onMount, tick } from 'svelte';
 	import MdInfoOutline from 'svelte-icons/md/MdInfoOutline.svelte';
 
@@ -15,10 +12,6 @@
 	const togglePricesInfoModalStatus = () => {
 		showPricesInfoModal = !showPricesInfoModal;
 	};
-
-	const revealTypes: Array<RevealType> = Object.values(RevealType).filter(
-		(entry) => typeof entry === 'number'
-	) as Array<RevealType>;
 
 	const closePricesInfoModalOnClickOutside = (
 		e: MouseEvent & {
@@ -55,14 +48,5 @@
   {hidden ? 'hidden' : ''} {showPricesInfoModal ? 'scale-100' : ''}"
 	bind:this={modal}
 >
-	<h2 class="text-lg">Reveal prices:</h2>
-	{#each revealTypes as revealType (revealType)}
-		<p class="my-1">
-			{revealNames[revealType]}:
-			<span class="text-blue-700 dark:text-blue-300">{revealPrices[revealType]}</span>
-		</p>
-	{/each}
-	<h2 class="text-lg">
-		Guessing prize: <span class="text-blue-700 dark:text-blue-300">{WIN_PRIZE}</span>
-	</h2>
+	<PricesInfoContent />
 </div>
---
+++ src/lib/shared/components/PricesInfoContent.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { revealNames } from '../constants/revealNames';
+	import { revealPrices } from '../constants/revealPrices';
+	import { WIN_PRIZE } from '../constants/winPrize';
+	import { RevealType } from '../model/enums/revealType';
+
+	const revealTypes: Array<RevealType> = Object.values(RevealType).filter(
+		(entry) => typeof entry === 'number'
+	) as Array<RevealType>;
+</script>
+
+<h2 class="text-lg">Reveal prices:</h2>
+{#each revealTypes as revealType (revealType)}
+	<p class="my-1">
+		{revealNames[revealType]}:
+		<span class="text-blue-700 dark:text-blue-300">{revealPrices[revealType]}</span>
+	</p>
+{/each}
+<h2 class="text-lg">
+	Guessing prize: <span class="text-blue-700 dark:text-blue-300">{WIN_PRIZE}</span>
+</h2>
