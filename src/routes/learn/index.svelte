3f69c49f8f4b5765eb7c78502eaa4cf5116187e2
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-11 11:47:08 +0200

    Styling adjustments

--- src/routes/learn/index.svelte
+++ src/routes/learn/index.svelte
@@ -1,15 +1,15 @@
-<section class="flex flex-col items-center justify-center">
-	<h1 class="text-center text-2xl max-w-lg">Goal of the game</h1>
-	<p class="text-center max-w-lg my-3">
+<section class="flex flex-col items-center justify-center max-w-lg mx-auto">
+	<h1 class="text-center text-2xl">Goal of the game</h1>
+	<p class="text-center my-3">
 		The goal is to get an as high score as possible by guessing the winner of the game with as
 		little information as possible. Gather the points through correct picking and then spend it to
 		gather various info in the next round. Some knowledge is more valuable than others, so costs
 		vary to balance everything out.
 	</p>
 </section>
-<section class="flex flex-col items-center justify-center">
-	<h1 class="text-center text-2xl max-w-lg">How to play?</h1>
-	<p class="text-center max-w-lg my-3">
+<section class="flex flex-col items-center justify-center max-w-lg mx-auto">
+	<h1 class="text-center text-2xl">How to play?</h1>
+	<p class="text-center my-3">
 		Use the reveal buttons to spend your score and get valuable pieces of information. If you are
 		not sure what is hiding below, there is a friendly tooltip for you. Hover it (or hold your
 		finger if you are on a touch device), and you will never be confused. At every point of the
@@ -18,9 +18,9 @@
 		guess. Click the button below the corresponding team to do it.
 	</p>
 </section>
-<section class="flex flex-col items-center justify-center">
-	<h1 class="text-center text-2xl max-w-lg">What games can you expect there?</h1>
-	<p class="text-center max-w-lg my-3">
+<section class="flex flex-col items-center justify-center max-w-lg mx-auto">
+	<h1 class="text-center text-2xl">What games can you expect there?</h1>
+	<p class="text-center my-3">
 		Games are taken from 5 servers - EUW, KR, NA, EUNE, and JP, from various ranks. The data size is
 		pretty generous. You can expect a variety of puzzles. Moreover, games are rotated every few
 		days, so you will never know what awaits behind a corner. Maybe you will even recognize your own
