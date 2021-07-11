6a61869bbaba4dc1db59de8f0ca487268871ea86
Paweł Błaszczyk <pawelblaszczyk@wir.pl>
2021-07-11 12:12:54 +0200

    Add changelog page

---
+++ src/routes/changelog/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Section from '$lib/shared/components/Section.svelte';
+</script>
+
+<Section>
+	<h1 class="text-center text-2xl">v1.0.0</h1>
+	<p class="text-center my-3">
+		The very first stable release of this game. Put your analytical skills to the test and try to
+		investigate who won the game with as little information as possible. Spend points you gathered
+		to get more clues but at the same time, try to get as high a score as possible. The balance -
+		costs and prize, is subject to change after I will get initial feedback.
+	</p>
+</Section>
