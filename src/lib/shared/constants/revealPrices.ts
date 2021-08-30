851854d29f550fa47e9127dd6a30e25c36c3a460
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2021-08-30 21:33:23 +0200

    Balance adjustments

--- src/lib/shared/constants/revealPrices.ts
+++ src/lib/shared/constants/revealPrices.ts
@@ -1,17 +1,17 @@
 import { RevealType } from '../model/enums/revealType';
 
 export const revealPrices: Record<RevealType, number> = {
-	[RevealType.DURATION]: 200,
-	[RevealType.CHAMPION_KILLS]: 300,
+	[RevealType.DURATION]: 250,
+	[RevealType.CHAMPION_KILLS]: 600,
 	[RevealType.TOWER_KILLS]: 750,
 	[RevealType.DRAGON_KILLS]: 500,
-	[RevealType.RIFT_HERALD_KILLS]: 300,
+	[RevealType.RIFT_HERALD_KILLS]: 450,
 	[RevealType.BARON_NASHOR_KILLS]: 800,
 	[RevealType.INHIBITOR_KILLS]: 950,
 	[RevealType.SUMMONERS]: 100,
 	[RevealType.RUNES]: 100,
-	[RevealType.CHAMPION_LEVEL]: 300,
-	[RevealType.ITEMS]: 400,
-	[RevealType.KDA]: 450,
-	[RevealType.GOLD]: 300
+	[RevealType.CHAMPION_LEVEL]: 350,
+	[RevealType.ITEMS]: 450,
+	[RevealType.KDA]: 500,
+	[RevealType.GOLD]: 400
 };
